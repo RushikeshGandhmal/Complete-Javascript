81c6ee433ca298b57f1c65de615f3f7b75674b43
Rushi <rushikeshgandhmal@gmail.com>
2023-08-12 19:58:57 +0530

    added proper comments

--- Fundamentals - 02/07. Arrays Operations.js
+++ Fundamentals - 02/07. Arrays Operations.js
@@ -4,28 +4,30 @@ const friends = ['Michael', 'Steven', 'Peter'];
 // Add elements
 const newLength = friends.push('Jay'); // push function returns lenght of a array after adding the new elements
 console.log(friends);
-console.log(newLength);
+console.log(newLength); // 4
 
-friends.unshift('John');
+// Add element to first
+const unshiftLen = friends.unshift('John');
+console.log(unshiftLen); // 5
 console.log(friends);
 
 // Remove elements
-friends.pop(); // Last
-const popped = friends.pop();
-console.log(popped);
+friends.pop(); // remove last element
+const popped = friends.pop(); // returns removed element
+console.log(popped); 
 console.log(friends);
 
-friends.shift(); // First
+friends.shift(); // remove first element
 console.log(friends);
 
-console.log(friends.indexOf('Steven'));
-console.log(friends.indexOf('Bob'));
+console.log(friends.indexOf('Steven')); // 1
+console.log(friends.indexOf('Bob')); // -1, not found
 
 friends.push(23);
-console.log(friends.includes('Steven'));
-console.log(friends.includes('Bob'));
-console.log(friends.includes(23));
+console.log(friends.includes('Steven')); // true
+console.log(friends.includes('Bob')); // false
+console.log(friends.includes(23)); // true
 
-if (friends.includes('Steven')) {
+if (friends.includes('Steven')) { //true
   console.log('You have a friend called Steven');
 }
