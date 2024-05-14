f39c514cfdc91f9964e474529bd2b4639dcb3aa4
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-05-14 16:41:26 +0530

    strings - part2

--- Data Structures, Modern Operators and Strings/07. strings.js
+++ Data Structures, Modern Operators and Strings/07. strings.js
@@ -12,3 +12,20 @@ console.log(airplane.lastIndexOf("A")); // 4
 console.log(airplane.slice(4, 7)); // Air
 console.log(airplane.slice(airplane.lastIndexOf(" ") + 1));
 console.log(airplane.split(" "));
+
+console.log(airplane.toLowerCase());
+console.log(airplane.toUpperCase());
+
+let passenger = "ruShi";
+passenger =
+  passenger.toLowerCase()[0].toUpperCase() + passenger.toLowerCase().slice(1);
+console.log(passenger); // Rushi
+
+const priceGB = "28,8,97£";
+const priceUS = priceGB.replaceAll(",", ".").replace("£", "$"); // replace doesn't mutate original string
+console.log(priceUS);
+
+const planeName = "A320neo";
+console.log(planeName.includes("A320")); // true
+console.log(planeName.startsWith("A")); // true
+console.log(planeName.endsWith("o")); // true
